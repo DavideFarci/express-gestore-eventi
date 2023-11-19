c69a8a77c6b28e8adf8cedcd1a081c7825e6c4c4
DavideFarci <yank92.df@gmail.com>
2023-11-19 14:05:33 +0100

    create model reservations

---
+++ models/reservation.js
@@ -0,0 +1,15 @@
+class Reservation {
+  firstName;
+  lastName;
+  email;
+  eventId;
+
+  constructor(firstName, lastName, email, eventId) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.eventId = eventId;
+  }
+
+  // FUNCTIONS -----------------------------------------
+}
