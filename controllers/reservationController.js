5b648af4841a37e4e47b060ca8c1856cea98d6b7
DavideFarci <yank92.df@gmail.com>
2023-11-19 14:04:57 +0100

    create controller reservations

---
+++ controllers/reservationController.js
@@ -0,0 +1,18 @@
+const { log } = require("console");
+const fs = require("fs");
+const path = require("path");
+
+const Event = require("../models/event");
+const EventError = require("../exeptions/eventExeption");
+
+function index(req, res) {}
+
+function store(req, res) {}
+
+function destroy(req, res) {}
+
+module.exports = {
+  index,
+  store,
+  destroy,
+};
