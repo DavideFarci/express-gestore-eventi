6abb4195b0529b3de6b2846d6364bafd3caf3498
DavideFarci <yank92.df@gmail.com>
2023-11-19 14:04:44 +0100

    create routes for reservations

--- routers/eventsRouter.js
+++ routers/eventsRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const eventController = require("../controllers/eventController");
+const reservationController = require("../controllers/reservationController");
+
+// EVENTS ---------------------------------------
 
 // INDEX
 router.get("/", eventController.index);
@@ -13,5 +16,9 @@ router.post("/", eventController.store);
 
 // UPDATE
 router.put("/:id", eventController.update);
+
+// RESERVATION ---------------------------------------
+// INDEX
+router.get("/:id/reservations", reservationController.index);
 
 module.exports = router;
